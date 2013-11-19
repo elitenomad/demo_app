1f4b7c8cf6c5c2b8ced5f790cc2c0600b1bc392a
pranava swaroop <pranavaswaroop@192-168-1-6.tpgi.com.au>
2013-11-19 17:58:35 +1100

    Initial Commit demo_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'bee874aa5b7c7d1e5540382f801a283faeb154e71546fa0db30fdf66b42835e8501a4d2c7b0840db32b1b3d12d2c70db0c04859240a79411b275bd763a10bc8f'
